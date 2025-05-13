4e3735e16b3e799419503766df8cb40fc7b84b6f
mbiplobmollah <biplobmollah16@gmail.com>
2025-05-13 23:51:41 +0600

    added date

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from '../assets/logo.png'
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
         <div className='flex justify-center gap-3 flex-col items-center'>
-            <img className='w-[350px]' src={logo} alt="logo" />
-            <p>Journalism Without Fear or Favour</p>
+            <img className='w-[400px]' src={logo} alt="logo" />
+            <p className='text-accent'>Journalism Without Fear or Favour</p>
+            <p className='font-semibold text-accent'>{format(new Date(),'EEEE , MMMM MM , yyyy')}</p>
         </div>
     );
 };
